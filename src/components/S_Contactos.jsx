3bdf765e29fb516c43e5e0ae5cabe80fa125bfed
Anibal Mancilla <mancillaanibal99@gmail.com>
2024-04-27 10:12:17 -0500

    section de contactos

--- src/components/S_Contactos.jsx
+++ src/components/S_Contactos.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import { Button } from './Button'
 
 const S_Contactos = () => {
   return (
-      <div className='Contactos relative'>
-          <div className='flex flex-row absolute right-11'>
+      <div className='Contactos'>
+          <div className='flex flex-row justify-end gap-4'>
             <div className='flex flex-col justify-center'>
               <h1 className='text-white text-5xl font-bold'>Servicio Delivery Gratuito</h1>
-              <p className='text-white'>Hasta su domicilio o dirección de oficina en zonas de cobertura</p>
-              <Button/>
+              <p className='text-white '>Hasta su domicilio o dirección de oficina en zonas de cobertura</p>
+              <span className='inline-block text-center'>
+                <Button title='CONTACTANOS' background={'bg-white'} clases={''} />
+              </span>
             </div>
             <div>
               <img src="s.png" alt="bidononcio" />
