58b9e0759318751a0cfafc05f4b8135cdab9a43a
Jeremy <jeremy_jair_2015@hotmail.com>
2024-05-05 20:06:54 -0500

    FormularioFuncional

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,93 @@
+import { useEffect, useState } from 'react';
 import '../Formulario.css'
 
+
+const datosEnvio = {
+
+    nombre: '',
+    apellido: '',
+    email:'',
+    telefono:'',
+    mensaje:''
+};
+
 export const Formulario = () => {
+    
+    const [formState, setFormState] = useState(datosEnvio);
+    
+    const {nombre, apellido ,email ,telefono,mensaje} = formState;
+
+    const [error, setError] = useState('');
+
+    function onSubmit(e){
+
+        e.preventDefault();
+
+        if (Object.values(formState).some(valor => valor === '')) {
+            
+            setError('HAY UN INPUT VACIO Y SEXOOOOOOOOOOOOOOOOOOOOOOOO');
+            return;
+        }
+
+
+        alert('SEXOOOOOOOOOOO ENVIADO');
+
+        setError('');
+
+        setFormState(datosEnvio);
+
+    }
+
+
+   const validacion = ({target}) =>{
+
+        const {name,value} = target;
+        
+        setFormState({
+            ...formState,
+            [name] : value
+        })
+
+
+   }
+
+   useEffect(()=>{
+
+    console.log('ACTIVANDO STATE!!!!!!!!!');
+
+   },[])
+
+   
+   useEffect(()=>{
+
+    console.log('ACTIVANDO nombre!!!!!!!!!');
+
+   },[nombre])
+
+   useEffect(()=>{
+
+    console.log('ACTIVANDO apellido!!!!!!!!!');
+
+   },[apellido])
+
+   useEffect(()=>{
+
+    console.log('ACTIVANDO email!!!!!!!!!');
+
+   },[email])
+
+   useEffect(()=>{
+
+    console.log('ACTIVANDO telefono!!!!!!!!!');
+
+   },[telefono])
+
+   useEffect(()=>{
+
+    console.log('ACTIVANDO mensaje!!!!!!!!!');
+
+   },[mensaje])
+
   return (
 
     <section>
@@ -37,38 +124,38 @@ export const Formulario = () => {
             </div>
                        
             <div className="form">
+                {error && <div className='bg-red-500 text-white p-2 font-bold'> {error}  </div> }
                 <h2>Comunicate con nosotros</h2>
-
-                <div className="form-box">
+                <form className="form-box" onSubmit={onSubmit}>
                     <div className="input-box w50">
-                            <input type="text" required/>
+                            <input type="text" onChange={validacion} name='nombre' value={nombre}/>
                             <span>Nombre</span>
                     </div>
 
                     <div className="input-box w50">
-                            <input type="text" required/>
+                            <input type="text" onChange={validacion} name='apellido' value={apellido}/>
                             <span>Apellido</span>
                     </div>
 
                     <div className="input-box w50">
-                            <input type="text" required/>
+                            <input type="text" onChange={validacion} name='email' value={email}/>
                             <span>Email</span>
                     </div>
 
                     <div className="input-box w50">
-                            <input type="text" required/>
+                            <input type="text" onChange={validacion} name='telefono' value={telefono}/>
                             <span>Telefono</span>
                     </div>
 
                     <div className="input-box w100">
-                            <textarea required></textarea>
+                            <textarea value={mensaje} onChange={validacion} name='mensaje'></textarea>
                             <span>Escribe tu mensaje...</span>
                     </div>
 
                     <div className="input-box w100 btn">
                             <input type="submit" value="Enviar"/>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </section>
