7ee79597dfc529d3b509d0e24cb78a5df6afdc24
Anibal Mancilla <mancillaanibal99@gmail.com>
2024-05-14 10:19:01 -0500

    responsive

--- src/components/S_Nosotros.jsx
+++ src/components/S_Nosotros.jsx
@@ -8,7 +8,7 @@ export function S_Nosotros() {
       <div className=" w-[70%] m-auto">
         <Titulo titulo={ 'Nosotros' } />
         <p className="text-center w-[60%] m-auto">Somos distribuidores exclusivos de diferentes marcas como son: AGUA SAN MATEO – AGUA JALSURI - AGUA SAN LUIS. Contamos con más de 20 años de experiencia en el mercado nacional.</p>
-        <div className="flex flex-row justify-center items-center gap-12">
+        <div className="flex flex-row justify-center items-center gap-12 max-lg:flex-col ">
           <div className="flex-col">
             <div className="mb-4">
               <h3 className="font-bold text-3xl mb-4">Vision</h3>
@@ -20,7 +20,7 @@ export function S_Nosotros() {
             </div>
           </div>
           <div className="w-full">
-            <img src="bolo.png" className="w-full h-80 object-cover" height={'470px'} />
+            <img src="bolo.png" className="w-full h-80 object-cover max-lg:object-right" />
           </div>
         </div>
       </div>
