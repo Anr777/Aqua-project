bea71da338f1cccf944ab5ff40a442c69e860028
Jeremy <jeremy_jair_2015@hotmail.com>
2024-05-25 22:59:00 -0500

    'fino'

--- src/components/Wasapp.jsx
+++ src/components/Wasapp.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export function Wasapp() {
   return (
-    <Link className="fixed w-14 bottom-6 right-6 z-20" target="_blank" to={'https://wa.me/945319118'}>
+    <Link className="fixed w-14 bottom-6 right-6 z-20" target="_blank" to={'https://web.whatsapp.com/send?phone=+51%20945319118&text=¡Hola! Jeremy,\n eres muy gosu la firme =D'}>
       <img src="/whatsapp5.png" />
       {/* <img src="/whatsapp3.png" /> */}
       {/* <img src="/whatsapp2.png" /> */}
