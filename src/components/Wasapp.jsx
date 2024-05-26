e4103b7be499a9cb0ed62f1f28b082d1961f1b78
Jeremy <jeremy_jair_2015@hotmail.com>
2024-05-25 23:42:32 -0500

    wsp responsive

--- src/components/Wasapp.jsx
+++ src/components/Wasapp.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export function Wasapp() {
   return (
-    <Link className="fixed w-14 bottom-6 right-6 z-20" target="_blank" to={'https://web.whatsapp.com/send?phone=+51%20945319118&text=¡Hola! Jeremy,\n eres muy gosu la firme =D'}>
+    <Link className="fixed w-14 bottom-6 right-6 z-20" target="_blank" to={'https://api.whatsapp.com/send?phone=+51%20945319118&text=¡Hola! Jeremy,\n eres muy gosu la firme =D'}>
       <img src="/whatsapp5.png" />
       {/* <img src="/whatsapp3.png" /> */}
       {/* <img src="/whatsapp2.png" /> */}
